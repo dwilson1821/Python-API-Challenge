643abd08f8e293923e51a517a56291e516e58500
dwilson1821 <dylonx.wilson@gmail.com>
2024-08-29 20:55:10 -0400

    Completed Mod 6 challenges

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "9bfd626b0613966d4248c46a22c4acea"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5114a9d32752412b88b487dc8fea7977"
